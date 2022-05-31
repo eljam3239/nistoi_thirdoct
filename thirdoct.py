e134c3d1fe09ce8adda6d3370a5c09f59060bd7c
eljam3239 <elijames2301@gmail.com>
2022-05-31 11:57:06 -0400

    Declare all properties

--- thirdoct.py
+++ thirdoct.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 """
 Computes third octave band matrix inptuts
 Inputs: 
@@ -15,5 +16,13 @@ def third_oct(fs, N_fft, numBands, mn):
     f = np.linspace(0, fs, N_fft+1)
     #f*1:n is the same as 1:f:n, right?
     f =np.mgrid[1: f:(N_fft/2)+1]
+    k = np.mgrid[0:numBands-1]
+    cf = np.power(2, k/3*mn)
+    #sqrt of a dot product I beleive
+    fl = math.sqrt(np.dot(np.power(2, k/3*mn), np.power(2, (k-1)/3*mn)))
+    fr = math.sqrt(np.dot(np.power(2, k/3*mn), np.power(2, (k+1)/3*mn)))
+
+    #this might need to be like m(f.shape) -- we want the size of this largest dimension?
+    A = np.zeros(numBands, f.max())
 
 
